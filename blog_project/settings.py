38b76244382fb1f94eda760594e2ff09c3f7f15d
Julio Briones <Julio_Briones@epam.com>
2022-05-26 19:51:27 -0500

    Added changes to requirements and decouple.

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^&w)agbxvwba&734&0fy&wr6gw-!tunfl9n71r@@)pm!o&h)44'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
